eb41e2f50777e644a69e2c0bddaf1f8fc0c4db41
Tuanguyen <tuanguyen94@gmail.com>
2016-06-07 17:34:09 +0700

    upload finished project

---
+++ winform_faceDetection/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace winform_faceDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imgCam = new Emgu.CV.UI.ImageBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnTakePic = new System.Windows.Forms.Button();
+            this.imgOut = new Emgu.CV.UI.ImageBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgCam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOut)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgCam
+            // 
+            this.imgCam.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imgCam.Location = new System.Drawing.Point(12, 52);
+            this.imgCam.Name = "imgCam";
+            this.imgCam.Size = new System.Drawing.Size(494, 489);
+            this.imgCam.TabIndex = 2;
+            this.imgCam.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 11);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(101, 31);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // btnTakePic
+            // 
+            this.btnTakePic.Location = new System.Drawing.Point(132, 11);
+            this.btnTakePic.Name = "btnTakePic";
+            this.btnTakePic.Size = new System.Drawing.Size(92, 30);
+            this.btnTakePic.TabIndex = 4;
+            this.btnTakePic.Text = "Take picture";
+            this.btnTakePic.UseVisualStyleBackColor = true;
+            this.btnTakePic.Click += new System.EventHandler(this.btnTakePic_Click);
+            // 
+            // imgOut
+            // 
+            this.imgOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imgOut.Location = new System.Drawing.Point(516, 53);
+            this.imgOut.Name = "imgOut";
+            this.imgOut.Size = new System.Drawing.Size(494, 488);
+            this.imgOut.TabIndex = 2;
+            this.imgOut.TabStop = false;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(256, 10);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(110, 30);
+            this.btnUpload.TabIndex = 5;
+            this.btnUpload.Text = "Upload a Image";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFileName.Location = new System.Drawing.Point(394, 11);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(299, 24);
+            this.lblFileName.TabIndex = 6;
+            this.lblFileName.Text = "Take a selfie or upload your image";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 554);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.imgOut);
+            this.Controls.Add(this.btnTakePic);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.imgCam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgCam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOut)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imgCam;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnTakePic;
+        private Emgu.CV.UI.ImageBox imgOut;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Label lblFileName;
+
+    }
+}
+
---
+++ winform_faceDetection/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.Cuda;
+using FaceDetection;
+
+namespace winform_faceDetection
+{
+    public partial class Form1 : Form
+    {
+        private Capture capture = null;        //takes images from camera as image frames
+        private bool captureInProgress; // checks if capture is executing
+
+        void Form1_Load(object sender, System.EventArgs e)
+        {
+        // Do whatever
+        }
+        public Form1()
+        {
+            InitializeComponent();  
+            CvInvoke.UseOpenCL = false;
+            try
+            {
+                //Create a capture using the default camera
+                capture = new Capture();
+
+                //The event to be called when an image is grabbed
+                capture.ImageGrabbed += RecordFrame;
+                //The event to be called when an image is grabbed:chụp
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+        }
+
+
+        private void RecordFrame(object sender, EventArgs arg)
+        {
+            Mat frame = new Mat();
+            capture.Retrieve(frame, 0);
+
+            //Đưa ảnh ra imageBox
+            imgCam.Image = frame;
+
+
+        }
+        private void ReleaseData()
+        {
+            if (capture != null)
+                capture.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (capture != null) capture.FlipVertical = !capture.FlipVertical;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (capture != null) capture.FlipHorizontal = !capture.FlipHorizontal;
+        }
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            
+            if (capture != null)
+            {
+                if (captureInProgress)
+                {  //stop the capture
+                    lblFileName.Text = "Recording from camera ..";
+                    btnStart.Text = "Start Capture";
+                    capture.Pause();
+                }
+                else
+                {
+                    //start the capture
+                    lblFileName.Text = "Take a selfie or upload your image";
+                    btnStart.Text = "Stop";
+                    capture.Start();
+                }
+
+                captureInProgress = !captureInProgress;
+            }
+        }
+
+        private Mat detectFace(Mat inputFrame)
+        {
+            Mat image = inputFrame; //Read the files as an 8-bit Bgr image  
+            long detectionTime;
+            List<Rectangle> faces = new List<Rectangle>();
+            List<Rectangle> eyes = new List<Rectangle>();
+
+            //The cuda cascade classifier doesn't seem to be able to load "haarcascade_frontalface_default.xml" file in this release
+            //disabling CUDA module for now
+
+            bool tryUseCuda = false;
+            bool tryUseOpenCL = true;
+
+
+            DetectFace.Detect(
+              image, "haarcascade_frontalface_default.xml", "haarcascade_eye.xml",
+              faces, eyes,
+              tryUseCuda,
+              tryUseOpenCL,
+              out detectionTime);
+
+
+            foreach (Rectangle face in faces)
+            {
+                CvInvoke.Rectangle(image, face, new Bgr(Color.Purple).MCvScalar, 3);
+                Bitmap c = inputFrame.Bitmap;
+                Bitmap bmp = new Bitmap(face.Size.Width, face.Size.Height);
+                Graphics g = Graphics.FromImage(bmp);
+                g.DrawImage(c, 0, 0, face, GraphicsUnit.Pixel);
+
+
+            }
+
+
+            foreach (Rectangle eye in eyes)
+                CvInvoke.Rectangle(image, eye, new Bgr(Color.Green).MCvScalar, 2);
+            return inputFrame;
+        }
+        private void btnTakePic_Click(object sender, EventArgs e)
+        {
+            Mat mat = new Mat();
+            capture.Retrieve(mat, 0);
+            //Image<Bgr, Byte> frame = mat.ToImage<Bgr, Byte>();
+
+            imgOut.Image = detectFace(mat);
+            
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFd = new OpenFileDialog();
+            openFd.Filter = "Images only. |*.jpg; *.jpeg;";
+            DialogResult dr = openFd.ShowDialog();
+            //Mat imgUpload = Image.FromFile(openFd.FileName);
+            lblFileName.Text = openFd.FileName;
+            Mat imgUpload = CvInvoke.Imread(openFd.FileName, Emgu.CV.CvEnum.LoadImageType.AnyColor);
+            imgOut.Image = detectFace(imgUpload);
+        }
+
+
+    }
+}
